919a009243be5efba43bd7a0bd6648663e3eeacd
Kelvin Rodriguez <kelvinrr@icloud.com>
2018-03-30 12:47:53 -0700

    updated bullet recipe

--- recipies/bullet/build.sh
+++ recipies/bullet/build.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
-
 mkdir bullet-build
 cd bullet-build
-cmake .. -G "Unix Makefiles" -DINSTALL_LIBS=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_INSTALL_RPATH=${PREFIX}
+
+cmake .. -G "Unix Makefiles" -DBUILD_PYBULLET=OFF \
+                             -DBUILD_PYBULLET_NUMPY=OFF \
+                             -DUSE_DOUBLE_PRECISION=ON \
+                             -DCMAKE_BUILD_TYPE=Release \
+                             -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+                             -DCMAKE_INSTALL_RPATH=${PREFIX}
+
 make -j ${CPU_COUNT}  || exit 1
 make install
