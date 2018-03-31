2dcb139d2016216549ca75d3004a6344a9b6a3bf
Kelvinrr <Kelvinrr@icloud.com>
2018-03-30 19:10:16 -0700

    updated bullet recipe to create shared libs

--- recipies/bullet/build.sh
+++ recipies/bullet/build.sh
@@ -2,6 +2,7 @@ mkdir bullet-build
 cd bullet-build
 
 cmake .. -G "Unix Makefiles" -DBUILD_PYBULLET=OFF \
+	                     -DBUILD_SHARED_LIBS=ON \
                              -DBUILD_PYBULLET_NUMPY=OFF \
                              -DUSE_DOUBLE_PRECISION=ON \
                              -DCMAKE_BUILD_TYPE=Release \
