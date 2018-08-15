a006d7e86b28131096fef71e4d153017396ecf80
chrisryancombs <ccombs@usgs.gov>
2018-08-15 14:10:43 -0700

    Added kakadu recipe.

---
+++ recipies/kakadu/build.sh
@@ -0,0 +1,9 @@
+#!/bin/tcsh
+cp /usgs/pkgs/local/v007/src/kakadu/v7_9_1-01762L.zip . 
+unzip v7_9_1-01762L.zip
+cd v7_9_1-01762L
+
+cd make
+# setenv CPLUS_INCLUDE_PATH /usr/lib/jvm/java-1.8.0/include:/usr/lib/jvm/java-1.8.0/include/linux
+make -f Makefile-Linux-x86-64-gcc
+rsync -rltpv ../lib/Linux-x86-64-gcc/* $PREFIX/lib/
