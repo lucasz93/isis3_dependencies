3de1ecd8532a6a4c8deab70c8d722949fc89da18
chrisryancombs <ryancombsjr@gmail.com>
2018-10-25 15:08:38 -0700

    Pin OpenCV version to 3.3.0

--- recipes/isis/build.sh
+++ recipes/isis/build.sh
@@ -1,6 +1,4 @@
 mkdir build
 cd build
-ls /usgs/apps/kakadu/v7_9_1-01762L/managed/all_includes/
 cmake -GNinja -DJP2KFLAG=ON -Dpybindings=OFF -DCMAKE_INSTALL_PREFIX=$PREFIX -Disis3Data=/usgs/cpkgs/isis3/data -Disis3TestData=/usgs/cpkgs/isis3/testData ../isis
 ninja install
-setisis .
